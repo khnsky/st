9637a5142fb154b4785b592e7f5c89fa5ebc135d
Piotrek Kochański <mailpiotrk@gmail.com>
2019-06-03 23:04:31 +0200

    change font

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "Inconsolata:pixelsize=14:style=bold:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
